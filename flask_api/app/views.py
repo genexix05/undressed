f1ca2008e2d397818c1705600b0f03aad4462c95
genexix05 <genexix05@gmail.com>
2024-05-13 18:19:35 +0200

    feat: Agregar soporte para ejecutar spiders de Scrapy desde la API Flask
    Este cambio permite ejecutar spiders de Scrapy desde la API Flask. Se ha agregado código para establecer el directorio correcto del proyecto Scrapy, agregar el directorio del proyecto a sys.path y ejecutar el comando Scrapy. También se ha actualizado el archivo de configuración scrapy.cfg para reflejar la nueva estructura del proyecto.

--- flask_api/app/views.py
+++ flask_api/app/views.py
@@ -1,4 +1,5 @@
-# app/views.py
+import os
+import sys
 from flask import Flask, jsonify
 import subprocess
 
@@ -7,8 +8,22 @@ app = Flask(__name__)
 @app.route('/run_spider/<spider_name>')
 def run_spider(spider_name):
     try:
-        # Asegúrate de que el comando y la ruta al ejecutable de scrapy sean correctos
-        subprocess.run(['scrapy', 'crawl', spider_name], check=True)
-        return jsonify({'status': 'success', 'message': f'Spider {spider_name} started successfully'})
-    except subprocess.CalledProcessError as e:
+        # Establecer el directorio correcto para Scrapy
+        project_path = os.path.abspath('../../spiders/stussy_spider')  # Ajusta según tu estructura
+        os.chdir(project_path)  # Cambiar al directorio del proyecto Scrapy
+
+        # Agregar el directorio del proyecto a sys.path
+        if project_path not in sys.path:
+            sys.path.append(project_path)
+
+        # Ejecutar el comando Scrapy
+        command = ['scrapy', 'crawl', spider_name]
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+
+        if process.returncode != 0:
+            return jsonify({'status': 'error', 'message': stderr.decode()})
+        else:
+            return jsonify({'status': 'success', 'message': stdout.decode()})
+    except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
