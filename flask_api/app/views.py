df565caea959cc46d3bf1136f055355abef017ed
genexix05 <genexix05@gmail.com>
2024-05-29 20:33:00 +0200

    Actualizar manejo de archivo JSON generado en el spider de Scrapy

--- flask_api/app/views.py
+++ flask_api/app/views.py
@@ -13,20 +13,23 @@ def run_spider(spider_name):
         os.chdir(project_path)  # Cambia al directorio del proyecto Scrapy
 
         # Añade el formato de salida JSON al comando
-        command = ['scrapy', 'crawl', spider_name, '-o', '-:json', '--nolog']
+        command = ['scrapy', 'crawl', spider_name, '-o', 'output.json', '--nolog']
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         stdout, stderr = process.communicate()
 
         if process.returncode == 0:
-            if stdout:
-                try:
-                    data = json.loads(stdout)  # Intenta parsear el JSON
-                    return jsonify({'status': 'success', 'data': data})
-                except json.JSONDecodeError:
-                    return jsonify({'status': 'error', 'message': 'No se pudo decodificar JSON', 'data': stdout})
-            else:
-                return jsonify({'status': 'error', 'message': 'No hay datos', 'stderr': stderr})
+            # Lee el archivo JSON generado
+            try:
+                with open('output.json', 'r', encoding='utf-8') as f:
+                    data = json.load(f)  # Carga el JSON
+                os.remove('output.json')  # Elimina el archivo JSON después de leerlo
+                return jsonify({'status': 'success', 'data': data})
+            except json.JSONDecodeError:
+                return jsonify({'status': 'error', 'message': 'No se pudo decodificar JSON', 'data': stdout})
         else:
             return jsonify({'status': 'error', 'message': 'Error ejecutando el spider', 'stderr': stderr})
     except Exception as e:
         return jsonify({'status': 'error', 'message': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- flask_api/spiders/stussy_spider/stussy.py
+++ flask_api/spiders/stussy_spider/stussy.py
@@ -9,6 +9,75 @@ from mysql.connector import Error
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
+
+
+class StussyProductsSpider(scrapy.Spider):
+    name = 'stussy'
+    allowed_domains = ['eu.stussy.com']
+    start_urls = ['https://eu.stussy.com/es-es/collections/all']
+
+    def __init__(self, category=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.category = category
+
+    def parse(self, response):
+        self.logger.info(f'Parsing page: {response.url}')
+        products = response.css('li.collection-grid__grid-item')
+        if not products:
+            self.logger.info('No products found on page')
+            return
+
+        for product in products:
+            yield {
+                'name': product.css('.product-card__title-link::text').get(),
+                'url': response.urljoin(product.css('.product-card__title-link::attr(href)').get()),
+                'price': product.css('.product-card__price span::text').get(),
+                'image_urls': product.css('.product-card__image img::attr(src)').getall(),
+                'sizes': [size.strip() for size in product.css('.product-card__size-variant-link::text').getall()],
+                'category': self.category
+            }
+
+        current_page_number = int(response.url.split('page=')[-1]) if 'page=' in response.url else 1
+        next_page_number = current_page_number + 1
+        next_page_url = f'https://eu.stussy.com/es-es/collections/all?page={next_page_number}'
+
+        if len(products) == 48:
+            self.logger.info(f'Following to next page: {next_page_url}')
+            yield response.follow(next_page_url, self.parse)
+
+class YeezySpider(scrapy.Spider):
+    name = 'yeezy'
+    allowed_domains = ['yeezy.com']
+    start_urls = ['https://yeezy.com/']
+
+    def parse(self, response):
+        # Extraer todos los bloques de estilo CSS y concatenarlos en una sola cadena de texto
+        styles = " ".join(response.css('style ::text').getall())
+        for product in response.css('.product-card'):
+            product_id = product.css('.product-card-image::attr(id)').get()
+            if product_id:
+                # Construir un patrón regex que coincida con el ID del producto específico y extraer las URLs de la imagen
+                pattern = rf'#{re.escape(product_id)}\s*{{.*?background-image:\s*url\((//.*?\.png).*?\)'
+                match = re.search(pattern, styles, re.DOTALL | re.IGNORECASE)
+                if match:
+                    # Añadir 'https:' al comienzo de la URL encontrada
+                    image_url = f"https:{match.group(1)}"
+                else:
+                    image_url = 'Imagen no disponible'
+            else:
+                image_url = 'Imagen no disponible'
+
+            yield {
+                'name': product.css('.product-card-title::text').get().strip(),
+                'price': product.css('.product-card-price::text').get(default='Precio no disponible').strip(),
+                'image_url': image_url,
+                'url': response.urljoin(product.css('::attr(href)').get()).strip(),
+            }
+
+        next_page = response.css('a.next::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
 @app.route('/run_spider/<spider_name>', methods=['GET', 'OPTIONS'])
 def run_spider(spider_name):
     if request.method == 'OPTIONS':
@@ -85,72 +154,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Methods', 'GET,OPTIONS')
     return response
 
-class StussyProductsSpider(scrapy.Spider):
-    name = 'stussy'
-    allowed_domains = ['eu.stussy.com']
-    start_urls = ['https://eu.stussy.com/es-es/collections/all']
 
-    def __init__(self, category=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.category = category
-
-    def parse(self, response):
-        self.logger.info(f'Parsing page: {response.url}')
-        products = response.css('li.collection-grid__grid-item')
-        if not products:
-            self.logger.info('No products found on page')
-            return
-
-        for product in products:
-            yield {
-                'name': product.css('.product-card__title-link::text').get(),
-                'url': response.urljoin(product.css('.product-card__title-link::attr(href)').get()),
-                'price': product.css('.product-card__price span::text').get(),
-                'image_urls': product.css('.product-card__image img::attr(src)').getall(),
-                'sizes': [size.strip() for size in product.css('.product-card__size-variant-link::text').getall()],
-                'category': self.category
-            }
-
-        current_page_number = int(response.url.split('page=')[-1]) if 'page=' in response.url else 1
-        next_page_number = current_page_number + 1
-        next_page_url = f'https://eu.stussy.com/es-es/collections/all?page={next_page_number}'
-
-        if len(products) == 48:
-            self.logger.info(f'Following to next page: {next_page_url}')
-            yield response.follow(next_page_url, self.parse)
-
-class YeezySpider(scrapy.Spider):
-    name = 'yeezy'
-    allowed_domains = ['yeezy.com']
-    start_urls = ['https://yeezy.com/']
-
-    def parse(self, response):
-        # Extraer todos los bloques de estilo CSS y concatenarlos en una sola cadena de texto
-        styles = " ".join(response.css('style ::text').getall())
-        for product in response.css('.product-card'):
-            product_id = product.css('.product-card-image::attr(id)').get()
-            if product_id:
-                # Construir un patrón regex que coincida con el ID del producto específico y extraer las URLs de la imagen
-                pattern = rf'#{re.escape(product_id)}\s*{{.*?background-image:\s*url\((//.*?\.png).*?\)'
-                match = re.search(pattern, styles, re.DOTALL | re.IGNORECASE)
-                if match:
-                    # Añadir 'https:' al comienzo de la URL encontrada
-                    image_url = f"https:{match.group(1)}"
-                else:
-                    image_url = 'Imagen no disponible'
-            else:
-                image_url = 'Imagen no disponible'
-
-            yield {
-                'name': product.css('.product-card-title::text').get().strip(),
-                'price': product.css('.product-card-price::text').get(default='Precio no disponible').strip(),
-                'image_url': image_url,
-                'url': response.urljoin(product.css('::attr(href)').get()).strip(),
-            }
-
-        next_page = response.css('a.next::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
 
 # Flask app runner
 if __name__ == '__main__':
